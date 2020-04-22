3f63e467a3c8f5fc14b86002d5269ccab1984651
Mikhail Mednikh <mmednikh@gmail.com>
2020-04-22 17:13:55 -0400

    finish with readme file

---
+++ Develop/README.md
@@ -0,0 +1,14 @@
+{
+  "username": "Mikhail1494",
+  "title": "",
+  "Description": "",
+  "contents": "",
+  "Installation": "",
+  "usage": "",
+  "license": "",
+  "contributing": "",
+  "tests": "",
+  "questions": "",
+  "image": "https://avatars2.githubusercontent.com/u/58613745?v=4",
+  "userEmail": null
+}
--- Develop/index.js
+++ Develop/index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer")
 const fs = require("fs");
-
+const axios = require("axios");
 //Question??? 
 
 //what is you GitHub name?
@@ -44,11 +44,6 @@ const questions = [
     },
     {
         type:"input",
-        message:"Your GitHub username?",
-        name:"username",
-    },
-    {
-        type:"input",
         message:"Your Contributing?",
         name:"contributing",
     },
@@ -61,28 +56,18 @@ const questions = [
         type:"input",
         message:"Your Questions?",
         name:"questions",
-    },
-    {
-        type:"input",
-        message:"Your  GitHub profile picture?",
-        name:"profilePicture",
-    },
-    {
-        type:"input",
-        message:"Your GitHub email?",
-        name:"email",
-    },
-
+    }
 ];
 
 
 // Expected output from GitHub API:
 //email and profile image 
 function writeToFile(fileName, data) {
-    fs.appendFile(fileName, data,(err) => console.log(err));
+    fs.appendFile(fileName, data,(err) => console.log("Success"));
+    // return console.log("Success")
     //generate a fiel 
     //connect with data
-
+}
 // At least one badge
 // Project title
 // Description
@@ -95,24 +80,27 @@ function writeToFile(fileName, data) {
 // Questions
 // User GitHub profile picture
 // User GitHub email
-}
+
+// {
+//     ...oldObject,
+//     key:value,
+//     key2: value2
+// }
 function init() {
     //user inquirer
-    inquirer.prompt(quesyions).then(response => { 
-        var image
-        var userEnail
+    inquirer.prompt(questions).then(response => { 
+        const oldReadMeData = {...response}
         axios.get("https://api.github.com/users/" + response.username)
     .then(response => {
-        image = response.data.avatar_url;
-        userEmail = response.data.email;
-        console.log(image);
-        console.log(email);
+        const newReadMeData = {...oldReadMeData, image:response.data.avatar_url, userEmail:response.data.email}
 
-    })
+        writeToFile("README.md", JSON.stringify(newReadMeData, null, 2))
+
+    });
 
 })
 .catch(function (err) {
     console.log(err)
 });
-
+}
 init();
